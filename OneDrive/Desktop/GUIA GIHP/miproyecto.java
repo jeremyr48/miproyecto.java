d0d42e2dc412a3799ef0e2b4dfdb541a4646ab25
Jeremy <jeremy.rodriguez805@std.uni.edu.ni>
2025-06-22 20:19:00 -0600

    primer commit: archivo miproyecto.java creado

---
+++ OneDrive/Desktop/GUIA GIHP/miproyecto.java
@@ -0,0 +1,15 @@
+public class miproyecto {
+    public static void main(String[]args){
+        String nombre = "estudiante";
+         
+        int a = 5;
+        int b = 3;
+
+        int suma = a  + b;
+
+
+        System.out.println("hola " + nombre);
+        System.out.println("La suma de " + a + " y" + b + " es:" + suma);
+    }
+
+}
