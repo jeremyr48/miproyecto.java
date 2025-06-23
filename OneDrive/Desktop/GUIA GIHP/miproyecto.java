b1303b53afd22c2c0c32f0b1de666c970f2f88df
Jeremy <jeremy.rodriguez805@std.uni.edu.ni>
2025-06-22 21:23:17 -0600

    tercer cambio confirmado

--- OneDrive/Desktop/GUIA GIHP/miproyecto.java
+++ OneDrive/Desktop/GUIA GIHP/miproyecto.java
@@ -2,14 +2,19 @@ public class miproyecto {
     public static void main(String[]args){
         String nombre = "estudiante";
          
-        int a = 30;
-        int b = 10;
+        int a = 5;
+        int b = 16;
 
-        int suma = a  + b;
+        int multiplicacion = a  * b;
+
+        System.out.println("Hola " + nombre);
+        System.out.println("La multiplicacion de " + a + " y " + b + " es: " + multiplicacion);
 
 
-        System.out.println("hola " + nombre);
-        System.out.println("La suma de " + a + " y" + b + " es:" + suma);
+
+       
+
+        
     }
 
 }
